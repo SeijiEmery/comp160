9f2ffd749b41157efa8a3774a0e9574081a26e12
Seiji Emery <seijiemery@gmail.com>
2016-10-15 17:41:39 -0700

    starting unit tests

--- asmlib/tests/osx32/asmlib_tests.asm
+++ asmlib/tests/osx32/asmlib_tests.asm
@@ -1,5 +1,46 @@
 
 %include "asmlib.inc"
+
+section .text
 global start
 start:
-    CALL_SYSCALL_EXIT -1
+    call runAllTests
+    CALL_SYSCALL_EXIT 0
+
+%macro ASSERT_EQ 3
+    cmp %1, %2
+    jz %%testOk
+    jnz %%testFail
+    section .data
+        %%msg: db %3
+        %%msg.length: equ $ - %%msg
+    section .text
+    %%testFail:
+        CALL_SYSCALL_WRITE STDOUT, %%msg, %%msg.length
+    %%testOk:
+%endmacro
+
+%macro WRITE_STR_LIT 1
+section .data
+    %%str: db %1
+    %%str.length: equ $ - %%str
+section .text
+    CALL_SYSCALL_WRITE STDOUT, %%str, %%str.length
+%endmacro
+
+runAllTests:
+    call test_sanity
+    call test_syscall_write_contract
+    ret
+
+test_sanity:
+    WRITE_STR_LIT {10,"Testing sanity. If this message does not appear, something broke.",10}
+    mov ebx, 2
+    add ebx, 2
+    ASSERT_EQ ebx, 4, {"This should pass",10}
+    ASSERT_EQ ebx, 5, {"This should fail",10}
+    ret
+
+test_syscall_write_contract:
+    mov ebx, esp
+    ret
