28eb1e96eadae9c3ddf06a8a0070639e32a8a9f0
Seiji Emery <seijiemery@gmail.com>
2016-11-07 16:44:21 -0800

    rewrote part 2

--- Assignment6/src/a6_pt2_win32.asm
+++ Assignment6/src/a6_pt2_win32.asm
@@ -6,129 +6,214 @@
 INCLUDE Irvine32.inc
 INCLUDE Macros.inc
 
-STRBUF_SIZE equ 1024
+
 .data
-    lit_optionsMenu BYTE "---- Boolean Calculator ----",13,10, \
-        "Enter:",13,10, \
-        "1. x AND y",13,10, \
-        "2. x OR y" ,13,10, \
-        "3. NOT x",13,10, \
-        "4. x XOR y",13,10, \
-        "5. Exit Program",13,10,0
+    ; Original options table. Now generated procedurally.
+
+    ; lit_optionsMenu BYTE "---- Boolean Calculator ----",13,10, \
+    ;     "Enter:",13,10, \
+    ;     "1. x AND y",13,10, \
+    ;     "2. x OR y" ,13,10, \
+    ;     "3. NOT x",13,10, \
+    ;     "4. x XOR y",13,10, \
+    ;     "5. Exit Program",13,10,0
+
+    ; Various strings for prompts, etc.
+
+    ; Menu prompt.
+    lit_menuHeader BYTE "--- Boolean Calculator ---",13,10,"Enter:",13,10,0
+    lit_menuSep1   BYTE ". ",0
+    lit_menuSep2   BYTE 13,10,0
     lit_promptStr  BYTE "> ",0
+
+    ; 
     lit_hexPrompt1 BYTE "Input the first 32-bit hexadecimal operand: ",0
     lit_hexPrompt2 BYTE "Input the second 32-bit hexadecimal operand: ",0
     lit_hexResult  BYTE "The 32-bit hexadecimal result is: ",0
 
-    ; Temp variables. Might as well store them here b/c a) convenient, b) using
-    ; the above memory, so will be in cache anyways.
-    var_currentOp DWORD 0
-    var_x         DWORD 0
-    var_y         DWORD 0
+    ; Operation strings (used in menu prompt, etc)
+    lit_and   BYTE "AND",0
+    lit_or    BYTE "OR",0
+    lit_not   BYTE "NOT",0
+    lit_xor   BYTE "XOR",0
+    lit_exit  BYTE "Exit Program",0
 
-    op_and  EQU 1
-    op_or   EQU 2
-    op_not  EQU 3
-    op_xor  EQU 4
-    op_exit EQU 5
+    ; Calculator op table. This procedurally defines which operations correspond
+    ; to what options, their semantics, etc.
+    calculatorOp_fcns  DWORD opAnd,   opOr,   opNot,   opXor,   opExit      ; function pointers
+    calculatorOp_argc  DWORD 2,       2,      1,       2,       0           ; # function args (eax, ebx)
+    calculatorOp_names DWORD lit_and, lit_or, lit_not, lit_xor, lit_exit    ; string names
+    NUM_OPS equ LENGTHOF calculatorOp_fcns
 
-    op_min EQU 1
-    op_max EQU 5    
+    ; Calculator internal run state
+    calculator_nextOp  DWORD ?
+    calculator_arg0    DWORD ?
+    calculator_arg1    DWORD ?
+    calculator_running DWORD ?  ; Set this to 0 to terminate the calculator.
 .code
 
-runBooleanCalculator PROC
-    push ebp       ; save stack frame
-    mov ebp, esp
+; Helper macro for indexed array reads. Equiv to c `dst = array[index]`.
+mReadArray MACRO dst:REQ, array:REQ index:REQ
+    mov dst, [array + index * TYPE array]
+mReadArray ENDM
 
-    ; Main loop -- repeat until we choose the exit option.
-    L1:
-        ; Display options
-        mov edx, OFFSET lit_optionsMenu
-        call WriteString
-
-        ; Ask user for integer corresponding to a menu option.
-        ; If invalid, will repeat this prompt until getting a valid result (0-5).
-        L2:
-            mov edx, OFFSET lit_promptStr
-            call WriteString
-            call ReadInt
-            call Crlf
-
-            cmp eax, op_min
-            jl invalidOption
-            cmp eax, op_max
-            jg invalidOption
-            jmp L2_end
-        invalidOption:
-
-            jmp L2
-        L2_end:
-        mov var_currentOp, eax
-
-        ; Handle exit op
-        cmp eax, op_exit
-        jz  L1_end
-
-        ; Prompt for first argument
-        mov edx, OFFSET lit_hexPrompt1
-        call WriteString
-        call ReadHex
-        mov  var_x, eax
-        call Crlf
-       
-        ; Skip second arg iff unary (op_not)
-        cmp var_currentOp, op_not
-        jz  execOpNot
-
-        ; Prompt for second argument
-        mov edx, OFFSET lit_hexPrompt2
-        call WriteString
-        call ReadHex
-        mov  var_y, eax
-        call Crlf
-
-        ;
-        ; Exec binary operation
-        ;
-
-        ; Load x / y values into eax / edx
-        xchg edx, eax
-        mov eax, var_x
-
-        ; Jump to corresponding op.
-        mov ebx, var_currentOp
-        cmp ebx, op_and
-        jz  execOpAnd
-        cmp ebx, op_or
-        jz  execOpOr
-        jmp execOpXor
-
-        execOpAnd:
-            and eax, edx
-            jmp printResult
-        execOpOr:
-            or eax, edx
-            jmp printResult
-        execOpNot:        
-            mov edx, -1   ; (not a) == (xor a, 0xFFFFFFFF)
-        execOpXor:
-            xor eax, edx
-            jmp printResult
-        printResult:
-            mov edx, OFFSET lit_hexResult
-            call WriteString
-            call WriteHex
-        call Crlf
-        call Crlf
-        jmp L1
-    L1_end:
-    mov esp, ebp   ; restore stack frame
-    pop ebp 
+;
+; Calculator operations. Very flexible -- just change this and the table
+; above to change calculator semantics / operations (menus will be updated, etc).
+;
+; Each operation is a function that takes N (0-2) integer operations
+; corresponding to calculatorOp_argc. eax = arg0, ebx = arg1.
+; Values undefined for arg[i] if i >= argc.
+;
+opAnd: and eax, ebx
     ret
-runBooleanCalculator ENDP
+opOr: or eax, ebx
+    ret
+opNot: xor eax, -1
+    ret
+opXor: xor eax, ebx
+    ret
+opExit: mov calculator_running, 0
+    ret
+
+;
+; Calculator entrypoint + run loop. Calls into everything else.
+;
+runCalculator PROC
+    mov calculator_running, 1
+    runLoop:
+        ; Get next option
+        call calculatorGetNextOption
+        mov calculator_nextOp, eax
+
+        ; Display which option the user chose
+        mWrite "executing operation "
+        mReadArray edx, calculatorOp_names, eax
+        call WriteString
+        mWrite ": ",13,10
+
+        ; call calculatorGetArgs to prompt user for ecx args and store
+        ; values into calculator_arg<n>.
+        mReadArray ecx, calculatorOp_argc, eax
+        call calculatorGetArgs
+
+        ; Execute next operation
+        mov edx, calculator_nextOp
+        mov eax, calculator_arg0
+        mov ebx, calculator_arg1
+        mReadArray edx, calculatorOp_fcns, edx
+        call edx
+
+        ; Check that we should still be running (if calculator_running set to 0, will terminate immediately)
+        cmp calculator_running, 0
+        jz  endRunLoop
+
+        ; Display result
+        mWriteString lit_hexResult
+        call WriteHex
+        jmp runLoop
+    endRunLoop:
+    ret
+runCalculator ENDP
+
+; calculatorGetArgs ( in ecx numArgs ) => prompt N times for hex values 
+; using lit_hexPrompt<n> + store in calculator_arg<n>.
+calculatorGetArgs PROC
+    dec ecx
+    jl  skipPrompt
+        mWriteString lit_hexPrompt1
+        call ReadHex
+        mov calculator_arg0, eax
+    dec ecx
+    jl  skipPrompt
+        mWriteString lit_hexPrompt2
+        call ReadHex
+        mov calculator_arg1, eax
+    skipPrompt:
+    ret
+calculatorGetArgs ENDP
+
+
+; Displays calculator menu + asks for a user menu option (repeats until it gets valid input).
+; Result stored in eax.
+calculatorGetNextOption PROC
+    ; Display menu.
+    call displayCalculatorMenu
+
+    ; Prompt for menu option.
+    ; Repeat until input falls within [min, max).
+    mov ebx, 1              ; min accepted value
+    mov ecx, NUM_OPS+1      ; max accepted value
+    mov edx, lit_promptStr  ; prompt string
+    call promptRange
+    ret
+calculatorGetNextOption ENDP
+
+
+; Displays text for menu options. Builds this procedurally based on calculatorOp_**** values.
+displayCalculatorMenu PROC
+    mov edx, lit_menuHeader
+    call WriteString
+
+    mov ecx, NUM_OPS
+    writeMenuOptions:
+        mov eax, NUM_OPS+1
+        sub eax, ecx
+        call WriteInt
+        mWrite ". "
+
+        mov eax, [calculatorOp_names + ecx * TYPE calculatorOp_names]
+        mov ebx, [calculatorOp_argc + ecx * TYPE calculatorOp_argc]
+        call writeBinaryOpStr
+        
+        mWriteLn
+        loop writeMenuOptions
+    ret
+displayCalculatorMenu ENDP
+
+
+; writeBinaryOp (in eax op_str, in ebx num_args)
+; writes:
+;    "x " op_str " y"  if num_args == 2
+;    op_str " x"       if num_args == 1
+;    op_str            if num_args == 0
+writeBinaryOpStr PROC
+    .if ebx >= 2
+        mWrite "x "
+        mWriteString eax
+        mWrite " y"
+    .else
+        mWriteString eax
+        .if ebx == 1
+            mWrite " x"
+        .endif
+    .endif
+    ret
+writeBinaryOpStr ENDP
+
+
+; Prompts user for an integer in [ebx, ecx), displaying a prompt string in edx each time.
+; Repeats until user input falls within this range
+promptRange PROC
+    doPrompt:
+        call WriteString
+        call ReadDec
+        jc   repeatPrompt
+        cmp  eax, ebx
+        jl   repeatPrompt
+        cmp  eax, ecx
+        jle  endPrompt
+    repeatPrompt:
+        call Crlf
+        jmp doPrompt
+    endPrompt:
+        call Crlf
+        ret
+promptRange ENDP
+
 
 main PROC
-    call runBooleanCalculator
+    call runCalculator
     exit
 main ENDP
 END main
